3e7ccdf2d0a3674f13cf97c491e0e2f4bf241ddc
regina <regina-will-programmieren@web.de>
2023-03-29 01:05:39 +0200

    Anfang modulares product erstellt

---
+++ src/js/product.js
@@ -0,0 +1,84 @@
+import "./product.css";
+
+const appContainer = document.querySelector("#app");
+
+// promises
+/*
+fetch("./public/products.json") // ask for json data
+  .then((response) => response.json()) // transform json to js object
+  .then((json) => {
+    // json is array of objects
+    json.forEach((product) => {
+      // create article
+      const article = document.createElement("article");
+
+      // create image
+      const productImage = document.createElement("img");
+      // set src attribute for image
+      productImage.setAttribute(
+        "src",
+        "https://images.unsplash.com/photo-1484994211335-48e240f0d140?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=774&q=80"
+      );
+      // append image to article
+      article.append(productImage);
+
+      // create product heading
+      const productTitle = document.createElement("h3");
+      // add class to title
+      productTitle.classList.add("title--blue");
+      productTitle.textContent = product.productName;
+      // append heading to article
+      article.append(productTitle);
+
+      // create price
+      const productPrice = document.createElement("span");
+      productPrice.textContent = product.price;
+      // append price to article
+      article.append(productPrice);
+
+      // append article to appContainer
+      appContainer.append(article);
+    });
+  });
+*/
+
+// async/await
+async function fetchProducts() {
+  const response = await fetch("./public/products.json");
+  const products = await response.json();
+  console.log(products);
+
+  /*
+  const productsHtml = products
+    .map((product) => {
+      const productHtml = `
+      <article>
+        <h3 class="title--blue">${product.productName}</h3>
+        <img src="https://images.unsplash.com/photo-1484994211335-48e240f0d140?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=774&q=80">
+        <div>${product.price}</div>
+      </article>
+    `;
+      return productHtml;
+    })
+    .join("");
+    */
+
+  const productsHtml = products
+    .map(
+      (product) => `
+    <a href="shop.html?id=${product.id}">
+     <article>
+       <h3 class="title--blue">${product.productName}</h3>
+       <img src="https://images.unsplash.com/photo-1484994211335-48e240f0d140?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=774&q=80">
+       <div>${product.price}</div>
+     </article>
+    </a>
+   `
+    )
+    .join("");
+
+  console.log(productsHtml);
+  appContainer.innerHTML = productsHtml;
+}
+
+fetchProducts();
