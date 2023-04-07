486d71ad0d7061bc414c0ac76b811741121e6690
regina <regina-will-programmieren@web.de>
2023-04-07 22:07:55 +0200

    slider white hinzugefügt

--- src/js/product.js
+++ src/js/product.js
@@ -64,6 +64,15 @@ async function fetchProducts() {
     .join("");
     */
 
+    const formatter = new Intl.NumberFormat('de-DE', {
+      style: 'currency',
+      currency: 'EUR',
+    
+      // These options are needed to round to whole numbers if that's what you want.
+      //minimumFractionDigits: 0, // (this suffices for whole numbers, but will print 2500.10 as $2,500.1)
+      //maximumFractionDigits: 0, // (causes 2500.99 to be printed as $2,501)
+    });
+
   const productsHtml = products
     .map(
       (product) => `
@@ -74,7 +83,7 @@ async function fetchProducts() {
                     ${product.productName}
                 </p>
                 <p class="price-product">
-                ${product.price}           
+                ${formatter.format(product.price / 100)}           
                 </p>
                 <p class="p-span">
                                 <span>
